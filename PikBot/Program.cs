3ee26b097b69b01f77699bfed55b1f0b4d72c61f
z-qi <g.qiao97@gmail.com>
2018-06-29 15:58:43 +1200

    Listens to non-bot messages with assigned prefix

--- PikBot/Program.cs
+++ PikBot/Program.cs
@@ -9,14 +9,17 @@ namespace PikBot
 {
     public class Program
     {
+        private class Credentials
+        {
+            public string prefix { get; set; }
+            public string token { get; set; }
+        }
+
+        Credentials credentials = JsonConvert.DeserializeObject<Credentials>(File.ReadAllText(@"./cred.json"));
+
         public static void Main(string[] args)
         {
             new Program().MainAsync().GetAwaiter().GetResult();
-        }
-
-        private class Credentials
-        {
-            public string token { get; set; }
         }
 
         public async Task MainAsync()
@@ -24,20 +27,33 @@ namespace PikBot
             var discordClient = new DiscordSocketClient();
 
             discordClient.Log += Log;
+            discordClient.MessageReceived += MessageReceived;
 
-            Credentials credentials = JsonConvert.DeserializeObject<Credentials>(File.ReadAllText(@"./cred.json"));
             string token = credentials.token;
 
             await discordClient.LoginAsync(TokenType.Bot, token);
             await discordClient.StartAsync();
 
-            await Task.Delay(-1);   
+            await Task.Delay(-1);
         }
 
         private Task Log(LogMessage msg)
         {
             Console.WriteLine(msg.ToString());
             return Task.CompletedTask;
+        }
+
+        private async Task MessageReceived(SocketMessage message)
+        {
+
+            if (!message.Author.IsBot && message.Content.StartsWith(credentials.prefix))
+            {
+                await message.Channel.SendMessageAsync("I'm listening");
+            }
+            else
+            {
+                return;
+            }
         }
     }
 }
