8e208cb5162e14fb753950580583abd32fa518de
z-qi <g.qiao97@gmail.com>
2018-07-04 21:30:34 +1200

    Added Commands list, Help, and Test commands

---
+++ PikBot/Commands/ActiveCommands/CommandsCommand.cs
@@ -0,0 +1,28 @@
+﻿using Discord.WebSocket;
+using PikBot.Bot;
+using System;
+using System.Threading.Tasks;
+
+namespace PikBot.Commands
+{
+    class CommandsCommand : Command
+    {
+        protected override string Desc { get { return "See a list of available commands"; } }
+
+        internal CommandsCommand(ISocketMessageChannel channel, User user, string[] args) : base(channel, user, args) { }
+
+        // Placeholder
+        protected override bool HasPermissions(User user) { return user.permissions.Contains(Permission.ALL); }
+
+        public override async Task Run()
+        {
+            if (HasPermissions(user))
+            {
+                string commandList = "```";
+                foreach (string commandName in Enum.GetNames(typeof(Commands))) commandList += commandName + ", ";
+
+                await channel.SendMessageAsync(commandList + "```");
+            }
+        }
+    }
+}
---
+++ PikBot/Commands/ActiveCommands/HelpCommand.cs
@@ -0,0 +1,36 @@
+﻿using Discord.WebSocket;
+using PikBot.Bot;
+using System.Threading.Tasks;
+
+namespace PikBot.Commands
+{
+    class HelpCommand : Command
+    {
+        protected override string Desc { get { return "Learn more about a command"; } }
+
+        internal HelpCommand(ISocketMessageChannel channel, User user, string[] args) : base(channel, user, args) { }
+
+        // Placeholder
+        protected override bool HasPermissions(User user) { return user.permissions.Contains(Permission.ALL); }
+
+        public override async Task Run()
+        {
+            if (HasPermissions(user))
+            {
+                if (args.Length > 0)
+                {
+                    char[] commandArray = args[1].ToLower().ToCharArray();
+                    commandArray[0] = char.ToUpper(commandArray[0]);
+                    string commandName = "PikBot.Commands." + new string(commandArray) + "Command";
+
+                    Command command = CommandFactory.GetCommandFactory().GetCommand(channel, user, new string[0], commandName);
+                    await command.HelpInfo();
+                }
+                else
+                {
+                    await channel.SendMessageAsync("Please include command name you need help with");
+                }
+            }
+        }
+    }
+}
