b44e1c3f6ff76157902357a5706612845d291c7a
z-qi <g.qiao97@gmail.com>
2018-07-11 14:24:53 +1200

    More reorganisation

--- PikBot/Commands/GeneralCommands.cs
+++ PikBot/Commands/GeneralCommands.cs
@@ -3,13 +3,21 @@ using System.Threading.Tasks;
 
 namespace PikBot.Commands
 {
-    public class EchoCommand : ModuleBase<SocketCommandContext>
+    public class GeneralCommands : ModuleBase<SocketCommandContext>
     {
         [Command("Echo")]
         [Summary("Echoes user's message")]
         public async Task Echo([Remainder] [Summary("The text to echo back")] string msg)
         {
             await ReplyAsync(msg);
+        }
+
+        [Command("RemindMe")]
+        [Alias("Remind", "RM")]
+        [Summary("Set up reminders")]
+        public async Task RemindMe([Remainder] string time)
+        {
+            await ReplyAsync("TODO");
         }
     }
 }
